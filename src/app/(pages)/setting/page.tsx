015c71ff188aeb3e543dd0ee6f3bd33039c0c1aa
takahashi.yuya <takahashi.yuya@dsic.jp>
2025-10-29 17:53:21 +0900

    ログアウト機能

--- src/app/(pages)/setting/page.tsx
+++ src/app/(pages)/setting/page.tsx
@@ -1,11 +1,18 @@
 "use client";
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 import { SettingForm } from "@/app/component/SettingForm";
 import { useUserStore } from '@/store/userStore'
+import { useRouter } from "next/navigation";
 
 export default function Setting() {
+  const router = useRouter();
+  const { user, logout: storeLogout } = useUserStore();
 
-  const { user } = useUserStore();
+  const logout=()=>{
+    storeLogout();
+    router.push("/login");
+  }
 
   return (
     <div className="">
@@ -19,6 +26,7 @@ export default function Setting() {
           priority
         />
         <SettingForm loginData={user} />
+        <Button variant="outline" onClick={logout}>LOGOUT</Button>
       </div>
     </div>
   );
